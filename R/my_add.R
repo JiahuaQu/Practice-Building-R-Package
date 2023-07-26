d2a4ae0bc38a47a83174a459549ddd25956e5c61
Jia-Hua Qu <jiahuaqu@hotmail.com>
2023-07-26 17:22:42 -0400

    first version of my package

---
+++ R/my_add.R
@@ -0,0 +1,12 @@
+#' Calculation of Sum
+#'
+#' @param x first number
+#' @param y second number
+#'
+#' @return sum of two numbers
+#' @export
+#'
+#' @examples add(1, 2)
+add <- function(x, y){
+  x + y
+}
